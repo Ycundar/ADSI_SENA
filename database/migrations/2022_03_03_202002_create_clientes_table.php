77dad1767b81892f90aad91def3bff49f387df32
Ycundar <ycundar@misena.edu.co>
2022-03-03 16:54:36 -0500

    Se Crean Migraciones

---
+++ database/migrations/2022_03_03_202002_create_clientes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nuip',);
+            $table->string('nombre_uno',80);
+            $table->string('nombre_dos',80);
+            $table->string('apellido_uno',80);
+            $table->string('apellido_dos',80);
+            $table->string('sexo',1);
+
+
+
+            $table->timestamps();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
